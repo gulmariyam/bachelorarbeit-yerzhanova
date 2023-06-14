fd60e4ec8685abbaf3da9a03b15893f609f3bae5
gulmariyam.yerzhanova <gulmariyam.yerzhanova@accso.de>
2023-06-14 23:21:12 +0200

    refactor[application]: Refactor application service with onion architecture

--- onion/src/main/java/de/hhu/accso/warenkorb/onion/application/services/ApplicationService.java
+++ onion/src/main/java/de/hhu/accso/warenkorb/onion/application/services/ApplicationService.java
@@ -1,14 +1,17 @@
 package de.hhu.accso.warenkorb.onion.application.services;
 
-import de.hhu.accso.warenkorb.onion.domain.repository.ArtikelRepository;
-import de.hhu.accso.warenkorb.onion.domain.repository.WarenkorbRepository;
 import de.hhu.accso.warenkorb.onion.domain.model.anzahl.Anzahl;
 import de.hhu.accso.warenkorb.onion.domain.model.artikel.Artikel;
 import de.hhu.accso.warenkorb.onion.domain.model.artikel.ArtikelID;
 import de.hhu.accso.warenkorb.onion.domain.model.kunde.KundeID;
+import de.hhu.accso.warenkorb.onion.domain.model.lager.Lager;
 import de.hhu.accso.warenkorb.onion.domain.model.preis.Preis;
 import de.hhu.accso.warenkorb.onion.domain.model.warenkorb.Warenkorb;
 import de.hhu.accso.warenkorb.onion.domain.model.warenkorb.WarenkorbID;
+import de.hhu.accso.warenkorb.onion.domain.repository.ArtikelRepository;
+import de.hhu.accso.warenkorb.onion.domain.repository.LagerRepository;
+import de.hhu.accso.warenkorb.onion.domain.repository.WarenkorbRepository;
+import de.hhu.accso.warenkorb.onion.domain.services.DomainService;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
@@ -16,13 +19,20 @@ import java.util.UUID;
 
 @Service
 public class ApplicationService {
+    private final DomainService domainService;
     private final WarenkorbRepository warenkorbRepository;
     private final ArtikelRepository artikelRepository;
+    private final LagerRepository lagerRepository;
 
 
-    public ApplicationService(WarenkorbRepository warenkorbRepository, ArtikelRepository artikelRepository) {
+    public ApplicationService(DomainService domainService,
+                              WarenkorbRepository warenkorbRepository,
+                              ArtikelRepository artikelRepository,
+                              LagerRepository lagerRepository) {
+        this.domainService = domainService;
         this.warenkorbRepository = warenkorbRepository;
         this.artikelRepository = artikelRepository;
+        this.lagerRepository = lagerRepository;
     }
 
     public void erstelleWarenkorbFuerKunde(UUID warenkorbID, UUID kundeID, BigDecimal preis) {
@@ -38,28 +48,42 @@ public class ApplicationService {
         return warenkorbRepository.findeMit(warenkorbID);
     }
 
+    public Artikel getArtikel(UUID artikel) {
+        ArtikelID artikelID = new ArtikelID(artikel);
+        return artikelRepository.findeMit(artikelID);
+    }
+
     public void legeArtikelInDenWarenkorb(UUID artikelId, int anzahl, UUID warenkorbID) {
         Warenkorb warenkorb = getWarenkorb(warenkorbID);
-        Artikel artikel = artikelRepository.findeMit(new ArtikelID(artikelId));
-        if (artikel == null) {
-            throw new IllegalArgumentException("Produkt existiert nicht.");
-        }
-        warenkorb.fuegeHinzu(artikel, new Anzahl(anzahl));
+        Artikel artikel = getArtikel(artikelId);
+        Lager lager = lagerRepository.findeMit(artikelId);
+
+        domainService.legeArtikelInDenWarenkorb(warenkorb, artikel, new Anzahl(anzahl), lager);
+
         warenkorbRepository.speichere(warenkorb);
+        lagerRepository.speichere(lager);
     }
 
     public void entferneArtikelAusDemWarenkorb(UUID artikelId, UUID warenkorbID) {
         Warenkorb warenkorb = getWarenkorb(warenkorbID);
-        Artikel artikel = artikelRepository.findeMit(new ArtikelID(artikelId));
-        warenkorb.entferne(artikel);
+        Artikel artikel = getArtikel(artikelId);
+        Lager lager = lagerRepository.findeMit(artikelId);
+
+        domainService.entferneArtikelAusDemWarenkorb(warenkorb, artikel, lager);
+
         warenkorbRepository.speichere(warenkorb);
+        lagerRepository.speichere(lager);
     }
 
 
     public void reduziereAnzahlVonArtikelInDemWarenkorb(UUID artikelId, int anzahl, UUID warenkorbID) {
         Warenkorb warenkorb = getWarenkorb(warenkorbID);
-        Artikel artikel = artikelRepository.findeMit(new ArtikelID(artikelId));
-        warenkorb.reduziere(artikel, new Anzahl(anzahl));
+        Artikel artikel = getArtikel(artikelId);
+        Lager lager = lagerRepository.findeMit(artikelId);
+
+        domainService.reduziereAnzahlVonArtikelInDemWarenkorb(warenkorb, artikel, new Anzahl(anzahl), lager);
+
         warenkorbRepository.speichere(warenkorb);
+        lagerRepository.speichere(lager);
     }
 }
